1812d336a36a8bb68d816399c054075a2b95bf10
florije1988 <florije1988@gmail.com>
2015-12-02 11:36:28 +0800

    添加依赖包。

--- com_service.py
+++ com_service.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
 
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
+from redis import Redis
+from celery import Celery
+import gunicorn
+
+print gunicorn.__version__
 
 app = Flask(__name__)
 
@@ -9,6 +15,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == '__main__':
