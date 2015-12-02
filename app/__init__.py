aa52099af962b484b038499d58bfd1e64afe2472
florije1988 <florije1988@gmail.com>
2015-12-02 14:42:16 +0800

    日志已经完备,稍后进行其他的处理。

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,14 @@ import logging
 import gunicorn
 from logging.handlers import TimedRotatingFileHandler
 from flask import Flask, send_from_directory
+from werkzeug.contrib.fixers import ProxyFix
 from redis import Redis
 from celery import Celery
 
 print gunicorn.__version__
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 # handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)  # TimedRotatingFileHandler
 handler = TimedRotatingFileHandler('logs/com_service.log', when='midnight', interval=1)
